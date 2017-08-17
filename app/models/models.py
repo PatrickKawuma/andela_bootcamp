73d21cbc22289c2431f57b2707f6194f3270bcad
rickka <rickonrails@gmail.com>
2017-08-17 16:30:57 +0300

    modified models.py and app.py

--- app/models/models.py
+++ app/models/models.py
@@ -1,7 +1,7 @@
 __author__ = "patrick"
 import uuid
 import datetime
-
+from flask import session
 '''
 class Item:
     def __init__(self, name, quantity, price):
@@ -10,9 +10,14 @@ class Item:
 	 self.price = price
 '''
 
+
+SHOPPING_LISTS = []
+USERS = []
+
+
 class ShoppingList:
     def __init__(self, list_name, budget, user): #takes arguments of list_name(str), budget(float) and a user object
-        self.name = name
+        self.name = list_name
         self.budget = budget
         self.items = {}
         self.created = datetime.datetime.today().strftime('%Y-%m-%d')
@@ -40,46 +45,55 @@ class User:
         self.created = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
         self.id = uuid.uuid4().hex
 
-    def get_user(self, username):
-        #search db for username
-        if user:
-            return user
+
+    @classmethod
+    def get_user(cls, username):
+        for user_object in USERS:
+            if user_object.username == username:
+                return user_object
+        return None
+
 
     @staticmethod
-    def login_valid(self, username_field, password_field):
-        if self.username == username_field and self.password == password_field:
-            return True
+    def login_valid(username_field, password_field):
+        for user in USERS:
+            user_exists = user.get_user(username_field)
+            if user_exists:
+                #confirm password
+                return user_exists.password == password_field
         return False
 
     @staticmethod
     def login(username):
-       #login_valid is called
-        session['email'] = username
-
+        session['username'] = username
 
     @classmethod
     def register(cls, username, password):
         user = cls.get_user(username)
         if user is None:
             new_user = cls(username, password)
-            #new_user.save()
+            USERS.append(new_user)
             session['username'] = username
+            new_user.login(new_user.username)
             return True
         return "user already exists"
 
     @staticmethod
     def logout():
+        #session.pop('username', None)
         session['email'] = None
 
-    def json(self):
-        return {
-            "username": self.username,
-            "password": self.password
-        }
-
-
-    def create_shoppinglist(self, list_name, items):
+    def create_list(self, list_name, items):
         return ShoppingList(list_name, budget, self.id)
+
+    def view_lists(self):
+        return [x for x in SHOPPING_LISTS if x.user_id == self.id]
+
+    def remove_list(self, list_name):
+        for list_object in SHOPPING_LISTS:
+            if list_object.name == list_name and list_object.user_id == self.user_id:
+                SHOPPING_LISTS.remove(list_object)
+
 
     def add_item(self, item_name, quantity, shopping_list):
         return shopping_list.add_item(item_name, quantity, price)
@@ -90,11 +104,11 @@ class User:
     def edit_item(self, shopping_list, data): #data expected as item_name, quantity and price
         return shopping_list.edit_item_data(**data)
 
-    '''
-    def remove_shoppinglist(self, list_name):
-   
 
+    '''
     def edit_shoppinglist(self, list_name):
+        for list_object in SHOPPING_LISTS:
+            if list_object.name == list_name and list_object.user_id == self.user_id:
 
     '''
 
